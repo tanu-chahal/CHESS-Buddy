92e9b2e2e6b65ce00f82bb35fea15046e520c0b7
Tanu Chahal <tanuchahal0004@gmail.com>
2023-08-23 10:44:07 +0530

    vite.config.js configured to resolve MIME issue.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    manifest: true,
+    rollupOptions: {
+      input: '/src/main.jsx', 
+    },
+  },
+});
